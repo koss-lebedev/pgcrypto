6ebe94e5cca2803ed5213d577e358979766698bb
koss.lebedev <koss.lebedev@gmail.com>
2015-01-03 17:41:26 +0500

    Fixed generator to make it work with Rails 4

--- lib/pgcrypto/generators/base_generator.rb
+++ lib/pgcrypto/generators/base_generator.rb
@@ -1,9 +1,7 @@
 require 'rails/generators/migration'
-require 'rails/generators/active_record/migration'
 
 class BaseGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
-  extend ActiveRecord::Generators::Migration
 
   def self.next_migration_number(*args)
     Time.now.utc.strftime("%Y%m%d%H%M%S").to_i.to_s
